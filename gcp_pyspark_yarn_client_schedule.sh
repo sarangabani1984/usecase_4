853a956e334d66966a5aa14e07f5fbab20f43fdc
sarangabani1984 <151030032+sarangabani1984@users.noreply.github.com>
2024-07-30 19:10:55 +0530

    Update gcp_pyspark_yarn_client_schedule.sh
    changed the path

--- gcp_pyspark_yarn_client_schedule.sh
+++ gcp_pyspark_yarn_client_schedule.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g","spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client","spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/inztechpvtltd/.git/spark/Usecase4_GcpGcsReadWritehive_cloud.py
+gcloud dataproc jobs submit pyspark --cluster=cluster-iz-dplr --region=us-central1 --properties="spark.driver.memory=2g","spark.executor.memory=2g","spark.executor.instances=4","spark.executor.cores=2","spark.submit.deployMode=client","spark.sql.shuffle.partitions=10","spark.shuffle.spill.compress=true" /home/sarangabani1984/project/Usecase4_GcpGcsReadWritehive_cloud.py
 if [ $? -ne 0 ]
 then
 echo "`date` error occured in the Pyspark job" > /tmp/gcp_pyspark_schedule.log
